c4026a5fe3c9b16ee3e1a084f2361832c0be1c76
Finn Rudolph <finn.rudolph@t-online.de>
2022-02-20 17:20:46 +0100

    Muellabfuhr: Implement Shortest Path Tour LB

--- muellabfuhr/fhk_algorithm.cpp
+++ muellabfuhr/fhk_algorithm.cpp
@@ -1,1 +1,12 @@
+#include "fhk_algorithm.hpp"
 
+int shortest_path_tour(adj_map &graph, matrix_2d &dis) {
+    int farthest = 0;
+    for (int i = 0; i < graph.size(); i++) {
+        for (auto it = graph[i].begin(); it != graph[i].end(); it++) {
+            farthest = max(dis[0][i] + it->second + dis[it->first][0], farthest);
+        }
+    }
+
+    return farthest;
+}
---
+++ muellabfuhr/fhk_algorithm.hpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+#include "types.hpp"
+using namespace std;
+
+int shortest_path_tour(adj_map &graph, matrix_2d &dis);
