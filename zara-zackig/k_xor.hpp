5a46c97e3dcf26e6aad3fbf1583728fa6f22cafe
Finn Rudolph <finn.rudolph@t-online.de>
2022-03-10 14:34:53 +0100

    Zara Zackig: Change data structure of used
    Replace vector with C-style array on stack memory for speed.

--- zara-zackig/k_xor.hpp
+++ zara-zackig/k_xor.hpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <stdint.h>
 #include <iostream>
+#include <cstring>
 #include "io.hpp"
 using namespace std;
 
@@ -12,30 +13,28 @@ template <typename T>
 void xor_combine(
     vector<T> &cards,
     int d,
-    function<void (T, vector<uint8_t>&)> cb,
-    vector<uint8_t> &used,
+    function<void (T)> cb,
+    uint8_t* used,
     T xor_val = 0,
     uint8_t start = 0
 ) {
     if (d == 0) {
-        cb(xor_val, used);
+        cb(xor_val);
         return;
     }
 
     for (uint8_t i = start; i < cards.size(); i++) {
-        xor_val = xor_val xor cards[i];
-        used.push_back(i);
+        xor_val ^= cards[i];
+        used[d - 1] = i;
         xor_combine<T>(cards, d - 1, cb, used, xor_val, i + 1);
-
-        xor_val = xor_val xor cards[i];
-        used.pop_back();
+        xor_val ^= cards[i];
     }
 }
 
-bool no_intersection(vector<uint8_t> &vec, uint8_t* arr, int length) {
-    unordered_set<uint8_t> arr_set(arr, arr + length);
-    for (uint8_t &number: vec) {
-        if (arr_set.find(number) != arr_set.end()) {
+bool no_intersection(uint8_t* arr1, uint8_t* arr2, int len1, int len2) {
+    unordered_set<uint8_t> arr_set(arr2, arr2 + len2);
+    for (int i = 0; i < len1; i++) {
+        if (arr_set.find(arr1[i]) != arr_set.end()) {
             return false;
         }
     }
@@ -94,25 +93,25 @@ void find_k_xor(int n, int k, int m) {
     T* values = new T[num_comb];
     uint8_t* indices = new uint8_t[num_comb * d];
     int length = 0;
-    vector<uint8_t> used;
+    uint8_t used[k - d];
 
     xor_combine<T>(cards, d, 
-        [&values, &indices, &length, &d](T xor_val, vector<uint8_t> &used) {
+        [&values, &indices, &used, &length, &d](T xor_val) {
             values[length] = xor_val;
-            move(used.begin(), used.end(), indices + length * d);
+            move(used, used + d, indices + length * d);
             length += 1;
         }, 
         used);
 
-    used.clear();
     radix_sort_msd<T>(values, indices, num_comb, d);
+
     set<vector<uint8_t>> results;
 
     xor_combine<T>(cards, k - d,
-        [&values, &indices, &results, &num_comb, &d](T xor_val, vector<uint8_t> &used) {
+        [&values, &indices, &results, &used, &num_comb, &k, &d](T xor_val) {
             int i = bs<T>(values, num_comb, xor_val);
-            if (i != -1 && no_intersection(used, indices + i * d, d)) {
-                vector<uint8_t> used_cards(used.begin(), used.end());
+            if (i != -1 && no_intersection(used, indices + i * d, k - d, d)) {
+                vector<uint8_t> used_cards(used, used + k - d);
                 used_cards.insert(used_cards.end(), indices + i * d, indices + i * d + d);
 
                 sort(used_cards.begin(), used_cards.end());
