a85c8e65856d111724c4a2985bbecfdd874a5d1d
Finn Rudolph <finn.rudolph@t-online.de>
2022-03-22 00:08:14 +0100

    Muellabfuhr: Fix bug at matching computation
    Weight was maximized rather than minimized.

--- muellabfuhr/perfect_matching.hpp
+++ muellabfuhr/perfect_matching.hpp
@@ -1,8 +1,8 @@
 #include <set>
 #include "types.hpp"
 
-std::set<std::pair<int, int>> perfect_matching(map_2d &graph, std::pair<int, int> &largest_edge);
+std::vector<edge> perfect_matching(map_2d &graph, edge &largest_edge);
 
 float circle_intersection(float r1, float r2, float distance);
 
-std::vector<edge> two_opt(map_2d &graph);
+std::vector<edge> two_opt(map_2d &graph, std::vector<edge> mat = { });
