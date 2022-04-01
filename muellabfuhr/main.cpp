84915f0dde25d1a3cdf9e8d08dc9b501aeeef124
Finn Rudolph <finn.rudolph@t-online.de>
2022-04-01 23:19:58 +0200

    Muellabfuhr: Add time measurement

--- muellabfuhr/main.cpp
+++ muellabfuhr/main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <chrono>
 #include "fhk_algorithm.hpp"
 #include "io.hpp"
 
@@ -16,10 +17,13 @@ int main(int argc, char* argv[]) {
     
     std::cout << "k = " << k << ", α = " << alpha << '\n';
 
+    auto begin = std::chrono::system_clock::now();
     int n, m; 
     std::cin >> n >> m;
 
     adj_map graph = to_adjacency_map(n, m);
     std::vector<std::vector<int>> tours = fhk(graph, k, alpha);
     print_tours(tours, graph);
+
+    std::cout << ((std::chrono::duration<float>) (std::chrono::system_clock::now() - begin)).count() << " s \n"; 
 }
